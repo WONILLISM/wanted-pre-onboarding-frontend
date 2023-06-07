c4039249e5e84f567ef58a76fc15677fcba63163
WONILLISM <wonillism@gmail.com>
2023-06-07 16:59:43 +0900

    [feat] add email password validation (#2)
    제어 컴포넌트를 사용한 Input
    정규식 표현을 사용한 validation

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,70 @@
+import { ChangeEvent, useState } from "react";
+import Input from "../components/Input";
+
+const SignIn = () => {
+  const [email, setEmail] = useState<string>("");
+  const [isValidEmail, setIsValidEmail] = useState<boolean>(false);
+  const [emailHelpText, setEmailHelpText] = useState<string>("");
+
+  const [password, setPassword] = useState<string>("");
+  const [isValidPassword, setIsValidPassword] = useState<boolean>(false);
+  const [passwordHelpText, setPasswordHelpText] = useState<string>("");
+
+  const emailRegex = /\S+@\S+/;
+  const passwordRegex = /^.{8,}$/;
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    if (emailRegex.test(value)) {
+      setIsValidEmail(true);
+      setEmailHelpText("올바른 이메일 형식입니다.");
+    } else {
+      setIsValidEmail(false);
+      setEmailHelpText("올바르지 않은 이메일 형식입니다.");
+    }
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    if (passwordRegex.test(value)) {
+      setIsValidPassword(true);
+      setPasswordHelpText("올바른 비밀번호 형식입니다.");
+    } else {
+      setIsValidPassword(false);
+      setPasswordHelpText("올바르지 않은 비밀번호 형식입니다.");
+    }
+    setPassword(value);
+  };
+
+  return (
+    <div>
+      <Input
+        data-testid="email-input"
+        label="email"
+        helpText={emailHelpText}
+        value={email}
+        type="email"
+        onChange={handleEmailChange}
+      />
+      <Input
+        data-testid="password-input"
+        label="password"
+        helpText={passwordHelpText}
+        value={password}
+        type="password"
+        onChange={handlePasswordChange}
+      />
+      <button
+        data-testid="signin-button"
+        disabled={!isValidEmail || !isValidPassword}
+      >
+        로그인
+      </button>
+    </div>
+  );
+};
+
+export default SignIn;
