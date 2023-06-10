5c4d5ee09e2b97d14621fa7a7564eb53a1332d69
WONILLISM <wonillism@gmail.com>
2023-06-10 16:28:18 +0900

    [feat] add delete and update button (#10)
    todo 수정 삭제 버튼 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -20,6 +20,8 @@ const TodoItem = ({ todo }: Props) => {
         <input type="checkbox" onChange={handleCompleteChange} />
         <span>{todo.todo}</span>
       </label>
+      <button data-testid="modify-button">수정</button>
+      <button data-testid="delete-button">삭제</button>
     </li>
   );
 };
